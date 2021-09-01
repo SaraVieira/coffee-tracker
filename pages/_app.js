03d434877bd77c5b681c8dd732293eb0ed7e8ec4
Sara Vieira <hey@iamsaravieira.com>
2021-09-02 01:25:02 +0200

    fix css

--- pages/_app.js
+++ pages/_app.js
@@ -3,20 +3,24 @@ import { UserContextProvider } from '../hooks/authUser'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import 'tailwindcss/tailwind.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { Hydrate } from 'react-query/hydration'
 import 'jdenticon'
 
 export default function MyApp({ Component, pageProps }) {
-  const queryClient = new QueryClient()
+  const [queryClient] = React.useState(() => new QueryClient())
+
   return (
-    <main className={'dark'}>
+    <>
       <style>{`.incognito-avatar svg{width: 100% !important; height: 100% !important;
         }`}</style>
       <QueryClientProvider client={queryClient}>
-        <UserContextProvider>
-          <Component {...pageProps} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </UserContextProvider>
+        <Hydrate state={pageProps.dehydratedState}>
+          <UserContextProvider>
+            <Component {...pageProps} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </UserContextProvider>
+        </Hydrate>
       </QueryClientProvider>
-    </main>
+    </>
   )
 }
--- views/roasters/main.js
+++ views/roasters/main.js
@@ -24,7 +24,6 @@ const RoastersComponent = ({ roasters: starterRoasters, user }) => {
           <main className="flex-1 overflow-y-auto">
             <div className="pt-8 max-w-7xl mx-auto">
               <Header type="roasters" user={user} />
-
               <section className="mt-8 pb-16" aria-labelledby="gallery-heading">
                 <ul className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                   {roasters.map((roaster) => (
